041563b92084f096ad27e44e09678ddf52fa9cdd
asset.b <asset.b@halykmarket.com>
2024-09-20 01:06:29 +0500

    new host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123', '1234567891123456789.zapto.org', 'localhost']
+ALLOWED_HOSTS = ['123.123.123.123', 'taskiddtaski8taski.zapto.org', 'localhost']
 
 
 # Application definition
